5e2f4aa8cb0dd4b0fe3aa98e79e140e465e8842a
xiaoxingping <xx9815@qq.com>
2018-05-22 16:42:44 +0800

    设置文章宽度

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-md-9">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="text-center">
