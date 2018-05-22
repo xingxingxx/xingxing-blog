57d4c8600041b56dd3ba929a9809b0081e16febf
xiaoxingping <xx9815@qq.com>
2018-05-22 09:53:24 +0800

    使代码高亮

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,68 +10,74 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('vendor/markdown/css/editormd.min.css')}}"/>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="border-bottom: 4px solid #336699;">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="border-bottom: 4px solid #336699;">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-                            {{--<li><a class="nav-link" href="{{ route('register') }}">注册</a></li>--}}
-                        @else
-                            <li><a class="nav-link" href="{{ route('create') }}">添加文章</a></li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                        {{--<li><a class="nav-link" href="{{ route('register') }}">注册</a></li>--}}
+                    @else
+                        <li><a class="nav-link" href="{{ route('create') }}">添加文章</a></li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
                                 </a>
 
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                       Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-        <footer style="text-align: center"><a target="_blank" href="http://www.miitbeian.gov.cn" style="color:#336699;">粤ICP备17155556号-2</a></footer>
-    </div>
+    <main class="py-4">
+        @yield('content')
+    </main>
+    <footer style="text-align: center"><a target="_blank" href="http://www.miitbeian.gov.cn" style="color:#336699;">粤ICP备17155556号-2</a>
+    </footer>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+@yield('script')
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,16 +8,41 @@
                     <div class="card-body">
                         <h5 class="text-center">{{ $article->title }}</h5>
                         <p class="text-center">发布时间：{{ $article->created_at }}</p>
-                        <p>{!! MarkdownEditor::parse($article->content) !!}</p>
+                        <p id="doc-content">
+                            <textarea style="display:none;"> {!! $article->content !!} </textarea>
+                        </p>
                         @if($article->pre)
-                            <p>上一篇：<a style="color:#336699;" href="{{ route('show',['id'=>$article->pre->id]) }}">{{ $article->pre->title }}</a></p>
+                            <p>上一篇：<a style="color:#336699;"
+                                      href="{{ route('show',['id'=>$article->pre->id]) }}">{{ $article->pre->title }}</a>
+                            </p>
                         @endif
                         @if($article->next)
-                            <p>下一篇：<a style="color:#336699;" href="{{ route('show',['id'=>$article->next->id]) }}">{{ $article->next->title }}</a></p>
+                            <p>下一篇：<a style="color:#336699;"
+                                      href="{{ route('show',['id'=>$article->next->id]) }}">{{ $article->next->title }}</a>
+                            </p>
                         @endif
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+<script src="{{asset('vendor/markdown/js/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/markdown/js/editormd.min.js')}}"></script>
+<script src="{{asset('vendor/markdown/lib/marked.min.js')}}"></script>
+<script src="{{asset('vendor/markdown/lib/prettify.min.js')}}"></script>
+@section('script')
+    <script type="text/javascript">
+      $(function () {
+        editormd.markdownToHTML("doc-content", {
+          htmlDecode: "style,script,iframe",
+          emoji: true,
+          taskList: true,
+          tex: true, // 默认不解析
+          flowChart: true, // 默认不解析
+          sequenceDiagram: true, // 默认不解析
+          codeFold: true,
+        });
+      });
+    </script>
 @endsection
