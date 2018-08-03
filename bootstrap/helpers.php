91359d227196c6baf2dc7c5f37f4a3e3c1a28d35
xiaoxingping <xx9815@qq.com>
2018-08-03 10:20:55 +0800

    改为https

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -15,7 +15,7 @@ if (!function_exists('get_cover')) {
     {
         $firstPic = '';
         if (preg_match('/!\[[^\]]*]\((http):\/\/[^\)]*\.(png|jpg)(.*)\)/i', $content, $img_match)) {
-            if (preg_match('/(http:\/\/)[^>]*?\.(png|jpg)/i', $img_match[0], $img_match_result)) {
+            if (preg_match('/(https:\/\/)[^>]*?\.(png|jpg)/i', $img_match[0], $img_match_result)) {
                 $firstPic = $img_match_result[0];
             }
         }
@@ -55,7 +55,7 @@ if (!function_exists('get_abstract')) {
         /**
          * 替换图片地址
          */
-        $patterns[] = '/!\[[^\]]*]\((http):\/\/[^\)]*\.(png|jpg|gif)(.*)\)/i';
+        $patterns[] = '/!\[[^\]]*]\((https):\/\/[^\)]*\.(png|jpg|gif)(.*)\)/i';
         $replacements[] = '[图片]';
 
 
